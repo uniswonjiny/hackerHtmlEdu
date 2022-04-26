185dfcedd57bc7f15847b73ca989e3e361ab089d
uni.swonjiny <uni.swonjiny@gmail.com>
2022-04-26 15:58:17 +0900

    메뉴선택표시이벤트1

--- main.js
+++ main.js
@@ -3,4 +3,9 @@ let mainMenu = "news";
 const menuMoveEvent = (menuVal) => {
   console.log(menuVal);
   mainMenu = menuVal;
+
+  const aLinkArr = document.querySelectorAll('.pagetop a');
+
+  aLinkArr.forEach(el => el.innerHTML === menuVal ?  el.className="topsel" :  el.className="");
+
 }
