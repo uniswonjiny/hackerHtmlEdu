90a75e85b6b32d039188fa53d3e3f59be7f56fef
uni.swonjiny <uni.swonjiny@gmail.com>
2022-04-27 15:38:01 +0900

    메뉴선택후 화면 변경영역 처리1-변경영역선택및 변경

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-let mainMenu = "news";
+let template = ''
 
 const menuMoveEvent = (menuVal) => {
-  console.log(menuVal);
-  mainMenu = menuVal;
-
+  // 메뉴영역 요소 가져오기
   const aLinkArr = document.querySelectorAll('.pagetop a');
-
+  // 메뉴영역 선택 관련 기능 선택한 메뉴만 두꺼운 하얀 글자로 표시
   aLinkArr.forEach(el => el.innerHTML === menuVal ?  el.className="topsel" :  el.className="");
+
+  // 변경할 화면 영역 요소 가져오기
+  let content = document.querySelector('.itemlist tbody');
+  // 선택한 메뉴에 따라 화면 영역 변경하기
+  content.innerHTML = menuVal;
 
 }
