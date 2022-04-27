4a0a8d0d4bc754ddd96d341df78efbbde8634b42
uni.swonjiny <uni.swonjiny@gmail.com>
2022-04-27 15:40:12 +0900

    메뉴선택후 화면 변경영역 처리2 - 화면내 html 생성

--- main.js
+++ main.js
@@ -1,4 +1,55 @@
-let template = ''
+let templagenews = '<tr class="athing" id="31176840"><td align="right" valign="top" class="title"><span class="rank">1.</span></td>      <td valign="top" class="votelinks"><center><a id="up_31176840" href="vote?id=31176840&amp;how=up&amp;goto=newest"><div class="votearrow" title="upvote"></div></a></center></td><td class="title"><a href="https://www.cnn.com/2022/04/26/success/fidelity-bitcoin-401k/index.html" class="titlelink" rel="nofollow">Fidelity will soon offer Bitcoin as an option in 401(k)s</a><span class="sitebit comhead"> (<a href="from?site=cnn.com"><span class="sitestr">cnn.com</span></a>)</span></td></tr>';
+    templagenews += '<tr><td colspan="2"></td><td class="subtext"><span class="score" id="score_31176840">2 points</span> by <a href="user?id=kungfudoi" class="hnuser">kungfudoi</a> <span class="age" title="2022-04-27T06:04:56"><a href="item?id=31176840">5 minutes ago</a></span> <span id="unv_31176840"></span> | <a href="hide?id=31176840&amp;goto=newest">hide</a> | <a href="https://hn.algolia.com/?query=Fidelity%20will%20soon%20offer%20Bitcoin%20as%20an%20option%20in%20401(k)s&amp;type=story&amp;dateRange=all&amp;sort=byDate&amp;storyText=false&amp;prefix&amp;page=0" class="hnpast">past</a> | <a href="item?id=31176840">discuss</a>              </td></tr>'
+    templagenews += '<tr class="athing" id="31176829"> <td align="right" valign="top" class="title"><span class="rank">2.</span></td>      <td valign="top" class="votelinks"><center><a id="up_31176829" href="vote?id=31176829&amp;how=up&amp;goto=newest"><div class="votearrow" title="upvote"></div></a></center></td><td class="title"><a href="https://medium.com/@parttimeben/we-often-dont-ask-the-right-questions-637d738cddd1" class="titlelink" rel="nofollow">We Often Don’t Ask the Right Questions</a><span class="sitebit comhead"> (<a href="from?site=medium.com/parttimeben"><span class="sitestr">medium.com/parttimeben</span></a>)</span></td></tr>'    
+    templagenews += '<tr><td colspan="2"></td><td class="subtext"><span class="score" id="score_31176829">2 points</span> by <a href="user?id=behnamoh" class="hnuser">behnamoh</a> <span class="age" title="2022-04-27T06:02:27"><a href="item?id=31176829">7 minutes ago</a></span> <span id="unv_31176829"></span> | <a href="hide?id=31176829&amp;goto=newest">hide</a> | <a href="https://hn.algolia.com/?query=We%20Often%20Don%E2%80%99t%20Ask%20the%20Right%20Questions&amp;type=story&amp;dateRange=all&amp;sort=byDate&amp;storyText=false&amp;prefix&amp;page=0" class="hnpast">past</a> | <a href="item?id=31176829">discuss</a>              </td></tr>'
+
+let templagepast = '<tr style="height:6px"></tr>';
+templagepast += '<tr><td colspan="2"></td><td>Stories from April 26, 2022 (UTC)</td></tr>';
+templagepast += '<tr style="height:9px"></tr>';
+templagepast += '<tr><td colspan="2"></td><td>Go back a <span class="hnmore"><a href="front?day=2022-04-25">day</a></span>, <span class="hnmore"><a href="front?day=2022-03-26">month</a></span>, or <span class="hnmore"><a href="front?day=2021-04-26">year</a></span>. Go forward a <span class="hnmore"><a href="front?day=2022-04-27">day</a></span>.</td></tr>';
+templagepast += '<tr style="height:14px"></tr>';
+templagepast += '<tr class="athing" id="31157969"><td align="right" valign="top" class="title"><span class="rank">1.</span></td>      <td valign="top" class="votelinks"><center><a id="up_31157969" href="vote?id=31157969&amp;how=up&amp;goto=front"><div class="votearrow" title="upvote"></div></a></center></td><td class="title"><a href="item?id=31157969" class="titlelink">Ask HN: What happened to Twitter poison pill?</a></td></tr>';
+templagepast += '<tr><td colspan="2"></td><td class="subtext"><span class="score" id="score_31157969">316 points</span> by <a href="user?id=rukshn" class="hnuser">rukshn</a> <span class="age" title="2022-04-25T17:22:21"><a href="item?id=31157969">1 day ago</a></span> <span id="unv_31157969"></span> | <a href="hide?id=31157969&amp;goto=front">hide</a> | <a href="item?id=31157969">284&nbsp;comments</a>              </td></tr>';
+
+let templagecomments = `<tr class="athing" id="31176901"><td class="ind"></td><td valign="top" class="votelinks"><center><a id="up_31176901" href="vote?id=31176901&amp;how=up&amp;goto=newcomments"><div class="votearrow" title="upvote"></div></a></center>    </td><td class="default"><div style="margin-top:2px; margin-bottom:-10px;"><span class="comhead">  <a href="user?id=scrapheap" class="hnuser">scrapheap</a> <span class="age" title="2022-04-27T06:18:55"><a href="item?id=31176901">0 minutes ago</a></span> <span id="unv_31176901"></span><span class="navs"> | <a href="item?id=31176693">parent</a> | <a href="context?id=31176901" rel="nofollow">context</a> | <a href="newcomments?next=31176865" aria-hidden="true">next</a></span><span class="onstory"> |  on: <a href="item?id=31176138">SELinux is unmanageable; just turn it off if it ge...</a></span>                  </span></div><br><div class="comment">           <span class="commtext c00">In my experience creating and applying new policies with audit2allow is almost always the wrong thing to do.  Most SELinux issues are either a SELinux boolean that needs enabling, or SELinux file type that needs changing.<p>The few times where you do need to create new policies then audit2allow is useful but still requires you to review and understand the changes it's going to make before applying them.</p><div class="reply"></div></span></div></td></tr><tr class="spacer" style="height:15px"></tr>`;
+templagecomments += `<tr class="athing" id="31176900">    <td class="ind"></td><td valign="top" class="votelinks">
+<center><a id="up_31176900" href="vote?id=31176900&amp;how=up&amp;goto=newcomments"><div class="votearrow" title="upvote"></div></a></center>    </td><td class="default"><div style="margin-top:2px; margin-bottom:-10px;"><span class="comhead">
+    <a href="user?id=lern_too_spel" class="hnuser">lern_too_spel</a> <span class="age" title="2022-04-27T06:18:35"><a href="item?id=31176900">0 minutes ago</a></span> <span id="unv_31176900"></span><span class="navs"> | <a href="item?id=31172924">parent</a> | <a href="context?id=31176900" rel="nofollow">context</a> | <a href="newcomments?next=31176865" aria-hidden="true">next</a></span><span class="onstory"> |  on: <a href="item?id=31170431">An update on the campaign to defend serious math e...</a></span>                  </span></div><br><div class="comment">
+            <span class="commtext c00">Boaler taught sixth and seventh grade math before becoming a professor of education. She might know how to teach pre-algebra concepts, but I do not believe she understands which high school math is most useful.<p>A lot of students in the US learn physics without calculus, which is basically useless. It's just a set of magic formulae to plug and chug. We should not go further in that direction but instead go in the other direction.
+        </p><div class="reply"></div></span></div></td></tr>`;
+let templageask = `<tr class="athing" id="31175448">
+<td align="right" valign="top" class="title"><span class="rank">1.</span></td>      <td valign="top" class="votelinks"><center><a id="up_31175448" href="vote?id=31175448&amp;how=up&amp;goto=ask"><div class="votearrow" title="upvote"></div></a></center></td><td class="title"><a href="item?id=31175448" class="titlelink">Ask HN: Where can I sell my apps?</a></td></tr>`;
+templageask += `<tr><td colspan="2"></td><td class="subtext">
+<span class="score" id="score_31175448">7 points</span> by <a href="user?id=willswire" class="hnuser">willswire</a> <span class="age" title="2022-04-27T02:06:56"><a href="item?id=31175448">4 hours ago</a></span> <span id="unv_31175448"></span> | <a href="item?id=31175448">1&nbsp;comment</a>              </td></tr>`
+
+let templageshow = '<tr style="height:6px"></tr>';
+templageshow += `<tr><td colspan="2"></td><td>
+Please read the <a href="showhn.html"><u>rules</u></a>. You can also
+browse the <a href="shownew"><u>newest</u></a> Show HNs.
+</td></tr>`;
+templageshow += `<tr style="height:12px"></tr>`;
+templageshow += `<tr class="athing" id="31154567">
+<td align="right" valign="top" class="title"><span class="rank">1.</span></td>      <td valign="top" class="votelinks"><center><a id="up_31154567" href="vote?id=31154567&amp;how=up&amp;goto=show"><div class="votearrow" title="upvote"></div></a></center></td><td class="title"><a href="https://lists.sh" class="titlelink">Show HN: Lists.sh – A Microblog for Lists</a><span class="sitebit comhead"> (<a href="from?site=lists.sh"><span class="sitestr">lists.sh</span></a>)</span></td></tr>`;
+templageshow += `<tr><td colspan="2"></td><td class="subtext">
+<span class="score" id="score_31154567">100 points</span> by <a href="user?id=qudat" class="hnuser">qudat</a> <span class="age" title="2022-04-25T13:44:02"><a href="item?id=31154567">6 hours ago</a></span> <span id="unv_31154567"></span> | <a href="item?id=31154567">23&nbsp;comments</a>              </td></tr>`;
+
+
+let templagejobs = '<tr style="height:6px"></tr>';
+templagejobs += `<tr><td></td><td><img src="s.gif" height="1" width="14"></td>     <td>
+These are jobs at YC startups. See more at
+<a href="https://www.ycombinator.com/jobs"><u>ycombinator.com/jobs</u></a>.
+</td></tr>`;
+templagejobs += `<tr style="height:14px"></tr>`;
+templagejobs += `<tr class="athing" id="31173116">
+<td align="right" valign="top" class="title"><span class="rank"></span></td>      <td></td><td class="title"><a href="https://www.finleycms.com/work-at-finley" class="titlelink" rel="nofollow">Finley (YC W21) is a B2B fintech hiring in sales and eng (US remote)</a><span class="sitebit comhead"> (<a href="from?site=finleycms.com"><span class="sitestr">finleycms.com</span></a>)</span></td></tr>`;
+templagejobs += `<tr><td colspan="2"></td><td class="subtext">
+<span class="age" title="2022-04-26T21:01:08"><a href="item?id=31173116">9 hours ago</a></span>      </td></tr>`;
+templagejobs += `<tr class="spacer" style="height:5px"></tr>`;
+templagejobs += `<tr class="athing" id="31169959">
+<td align="right" valign="top" class="title"><span class="rank"></span></td>      <td></td><td class="title"><a href="https://www.ycombinator.com/companies/runops/jobs/4S6TgVQ-data-privacy-devops-advocate" class="titlelink" rel="nofollow">Runops (YC W21) is hiring a dev advocate to help fix security tech</a><span class="sitebit comhead"> (<a href="from?site=ycombinator.com"><span class="sitestr">ycombinator.com</span></a>)</span></td></tr>`;
+templagejobs += `<tr><td colspan="2"></td><td class="subtext">
+<span class="age" title="2022-04-26T17:00:54"><a href="item?id=31169959">13 hours ago</a></span>      </td></tr>`;
+
 
 const menuMoveEvent = (menuVal) => {
   // 메뉴영역 요소 가져오기
@@ -9,6 +60,29 @@ const menuMoveEvent = (menuVal) => {
   // 변경할 화면 영역 요소 가져오기
   let content = document.querySelector('.itemlist tbody');
   // 선택한 메뉴에 따라 화면 영역 변경하기
-  content.innerHTML = menuVal;
+  switch (menuVal) {
+    case 'news':
+      content.innerHTML = templagenews;
+      break;
+    case 'past':
+      content.innerHTML = templagepast;
+      break;
+    case 'comments':
+      content.innerHTML = templagecomments;
+      break;
+    case 'ask':
+      content.innerHTML = templageask;
+      break;
+    case 'show':
+      content.innerHTML = templageshow;
+      break;
+    case 'jobs':
+      content.innerHTML = templagejobs;
+      break;
+    default:
+      content.innerHTML = '';
+      break;
+  }
 
 }
+
